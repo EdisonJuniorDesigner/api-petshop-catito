832895444bb3cd92bc6acdec02a71835c87e74f1
EdisonJuniorDesigner <mage1232010@gmail.com>
2021-10-26 23:34:10 -0300

    persistindo dados, criando método post

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,11 +1,21 @@
 const roteador = require('express').Router()
 const TabelaFornecedor = require('./TabelaFornecedor')
+const Fornecedor = require('./Fornecedor')
 
-roteador.use('/', async (requisicao, resposta) => {
+// Lista todos os fornecedores
+roteador.get('/', async (requisicao, resposta) => {
     const resultados = await TabelaFornecedor.listar()
     resposta.send(
         JSON.stringify(resultados)
     )
+})
+
+// Adiciona fornecedores
+roteador.post('/', async (requisicao, resposta) => {
+    const dadosRecebidos = requisicao.body
+    const fornecedor = new Fornecedor(dadosRecebidos)
+    await fornecedor.criar()
+    resposta.send(JSON.stringify(fornecedor))
 })
 
 module.exports = roteador
