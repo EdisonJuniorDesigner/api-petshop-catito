cfdf26072950de8b5a01dfae8248a205780247fe
EdisonJuniorDesigner <mage1232010@gmail.com>
2021-11-04 00:57:28 -0300

    Criando rota para listar fornecedores pelo id

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -18,4 +18,17 @@ roteador.post('/', async (requisicao, resposta) => {
     resposta.send(JSON.stringify(fornecedor))
 })
 
+roteador.get('/:idFornecedor', async (requisicao, resposta) => {
+    try {
+        const id = requisicao.params.idFornecedor
+        const fornecedor = new Fornecedor({ id: id })
+        await fornecedor.carregar()
+        resposta.send(JSON.stringify(fornecedor))
+    } catch (erro) {
+        resposta.send(JSON.stringify({
+            mensagem: erro.message
+        }))
+    }
+})
+
 module.exports = roteador
